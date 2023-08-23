f22a5f3d117b20139262ea3de94dc403f8b53a24
Kawa Tai 119 <58183232+kawatainhat02@users.noreply.github.com>
2023-08-23 12:46:20 +0900

    Update Matlab.m

--- Matlab+Mercury.m
+++ Matlab+Mercury.m
@@ -1,27 +1,3 @@
 type hello_world
 codegen hello_world
 hello_world_mex
-
-add10(N, M) :- M = N + 10.
-:- module (モジュール名).
-:- interface.
-(外部に公開する述語の宣言)
-:- implementation.
-(外部に公開しない述語の宣言と述語の実装)
-:- end_module (モジュール名).
-
-%
-
-% hello.m
-:- module hello.
-
-:- interface.
-:- import_module io.
-:- pred main(io::di, io::uo) is det.
-
-:- implementation.
-main(IO1, IO2) :-
-    write_string("Hello, World!\n", IO1, IO2).
-
-:- end_module hello.
-
